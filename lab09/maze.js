e60dcdf016dd26e583be5f9a89696ee3bc6568fe
hssong1221 <hssong1221@naver.com>
2019-12-05 23:42:46 +0900

    modify lab09

--- lab09/maze.js
+++ lab09/maze.js
@@ -6,10 +6,9 @@
 
 var loser = null;  // whether the user has hit a wall
 var click = false;
+var win = false;
 
-document.observe("keydown", function(){
-    alert("준영 ㅄ ");
-})
+
 window.onload = function() {
     var end = $("end");
     end.onmouseover = overEnd;
@@ -28,7 +27,7 @@ window.onload = function() {
 // signals the end of the game with a loss
 function overBoundary(event) 
 {
-    if(loser === null)
+    if(loser === null && win === false)
     {
         loser = true;
         var boundary = $$("div#maze div.boundary");
@@ -44,28 +43,32 @@ function overBoundary(event)
 // sets the maze back to its initial playable state
 function startClick() {
     loser = null;
-    click = true;
-    
+    win = false;
+
     $("status").textContent = "Click the \"S\" to begin.";
 
     var boundary = $$("div#maze div.boundary");
     for(var k = 0 ; k < boundary.length; k++){
         boundary[k].removeClassName("youlose");
     }
-
+    document.body.observe("mouseover", overBody);
 }
 
 // called when mouse is on top of the End div.
 // signals the end of the game with a win
 function overEnd() {
-    $("status").textContent = "You Win :)";
-    event.stop();
+    if(win === false && loser === null)
+    {
+        win = true;
+        $("status").textContent = "You Win :)";  
+        event.stop();
+    }
 }
 
 // test for mouse being over document.body so that the player
 // can't cheat by going outside the maze
 function overBody(event) {
-    if(loser === null )
+    if(loser === null && event.element() == document.body)
     {
         overBoundary(event);
     }
